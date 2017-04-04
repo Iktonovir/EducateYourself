e30550a056655751b73e87595297e9e97a9aba12
Iktonovir <yavorkonov@gmail.com>
2017-04-05 00:46:30 +0300

    Modified MainActivity Added Test1stFile

--- app/src/main/java/com/example/user/educateyourselfquiz/MainActivity.java
+++ app/src/main/java/com/example/user/educateyourselfquiz/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
@@ -11,7 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
-    int scoreQ1, scoreQ2, scoreQ3, scoreQ4, scoreQ5, scoreQ6 = 0;
+    int scoreQ1, scoreQ2, scoreQ3, scoreQ4, scoreQ5, scoreQ6, scoreQ7, scoreQ8 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
         EditText nameField = (EditText) findViewById(R.id.name_field);
         String name = nameField.getText().toString();
+
+        EditText autismField = (EditText) findViewById(R.id.autism);
+        String autism = autismField.getText().toString();
 
         RadioButton thoughtsButton = (RadioButton) findViewById(R.id.thoughts_button);
         boolean thoughtsButtonChecked = thoughtsButton.isChecked();
@@ -75,99 +79,149 @@ public class MainActivity extends AppCompatActivity {
         RadioButton starButton = (RadioButton) findViewById(R.id.star_button);
         boolean starButtonChecked = starButton.isChecked();
 
-        if (thoughtsButtonChecked && organicButtonChecked) {
+        CheckBox pneumoniaCheckBox = (CheckBox) findViewById(R.id.pneumonia);
+        boolean hasPneumonia = pneumoniaCheckBox.isChecked();
+
+        CheckBox hpvCheckBox = (CheckBox) findViewById(R.id.hpv);
+        boolean hasHPV = hpvCheckBox.isChecked();
+
+        CheckBox syphilisCheckBox = (CheckBox) findViewById(R.id.syphilis);
+        boolean hasSyphilis = syphilisCheckBox.isChecked();
+
+        if (autism.equals(getString(R.string.Q1_no)) || autism.equals(getString(R.string.Q1_Big_no))){
+            scoreQ1 = 1;
+
+        } else {
             scoreQ1 = 0;
         }
 
-        if (vaccineButtonChecked) {
-            scoreQ1 = 1;
-        }
-
-        if (steveDeathButtonChecked) {
+        if (thoughtsButtonChecked && organicButtonChecked) {
             scoreQ2 = 0;
         }
 
-        if (noButtonChecked) {
+        if (vaccineButtonChecked) {
             scoreQ2 = 1;
         }
 
-        if (hepcButtonChecked && anemiaButtonChecked) {
+        if (steveDeathButtonChecked) {
             scoreQ3 = 0;
         }
 
-        if (hepabButtonChecked) {
+        if (noButtonChecked) {
             scoreQ3 = 1;
         }
 
-        if (meteorButtonChecked) {
+        if (hepcButtonChecked && anemiaButtonChecked) {
             scoreQ4 = 0;
         }
 
-        if (paltrowButtonChecked) {
+        if (hepabButtonChecked) {
             scoreQ4 = 1;
         }
 
-        if (kissingButtonChecked && foodButtonChecked) {
+        if (meteorButtonChecked) {
             scoreQ5 = 0;
         }
 
-        if (sexButtonChecked) {
+        if (paltrowButtonChecked) {
             scoreQ5 = 1;
         }
 
-        if (deadlyButtonChecked && starButtonChecked) {
+        if (kissingButtonChecked && foodButtonChecked) {
             scoreQ6 = 0;
         }
 
-        if (crisprButtonChecked) {
+        if (sexButtonChecked) {
             scoreQ6 = 1;
         }
 
-        score = scoreQ1 + scoreQ2 + scoreQ3 + scoreQ4 + scoreQ5 + scoreQ6;
+        if (deadlyButtonChecked && starButtonChecked) {
+            scoreQ7 = 0;
+        }
 
-        if (score == 6)
+        if (crisprButtonChecked) {
+            scoreQ7 = 1;
+        }
+
+        if (hasPneumonia && hasSyphilis){
+            scoreQ8 = 2;
+
+        } else if (hasPneumonia){
+            scoreQ8 = 1;
+
+        } else if (hasSyphilis){
+            scoreQ8 = 1;
+
+        } else {
+            scoreQ8 = 0;
+        }
+
+        score = scoreQ1 + scoreQ2 + scoreQ3 + scoreQ4 + scoreQ5 + scoreQ6 + scoreQ7 + scoreQ8;
+
+        if (score == 9)
 
         {
             Toast.makeText(this, name + getString(R.string.your_score_is)
                     + " " + score + getString(R.string.excellent), Toast.LENGTH_LONG).show();
         }
 
-        if (score == 5)
+        if (score == 8)
 
         {
             Toast.makeText(this, name + getString(R.string.your_score_is)
                     + " " + score + getString(R.string.very_good), Toast.LENGTH_LONG).show();
         }
 
-        if (score == 4)
+        if (score == 7)
 
         {
             Toast.makeText(this, name + getString(R.string.your_score_is)
                     + " " + score + getString(R.string.good), Toast.LENGTH_LONG).show();
         }
 
-        if (score == 3)
+        if (score == 6)
 
         {
             Toast.makeText(this, name + getString(R.string.your_score_is)
                     + " " + score + getString(R.string.not_bad), Toast.LENGTH_LONG).show();
         }
 
-        if (score == 2)
+        if (score == 5)
 
         {
             Toast.makeText(this, name + getString(R.string.your_score_is)
                     + " " + score + getString(R.string.should_be_better), Toast.LENGTH_LONG).show();
         }
 
-        if (score == 1)
+        if (score == 4)
 
         {
             Toast.makeText(this, name + getString(R.string.your_score_is)
                     + " " + score + getString(R.string.educate_yourself), Toast.LENGTH_LONG).show();
         }
 
-        if (score == 0)
+        if (score == 3)
+
+        {
+            Toast.makeText(this, name + getString(R.string.your_score_is)
+                    + " " + score + getString(R.string.much_to_read), Toast.LENGTH_LONG).show();
+        }
+
+        if (score == 2)
+
+        {
+            Toast.makeText(this, name + getString(R.string.your_score_is)
+                    + " " + score + getString(R.string.visit_a_library), Toast.LENGTH_LONG).show();
+        }
+
+        if (score == 1)
+
+        {
+            Toast.makeText(this, name + getString(R.string.your_score_is)
+                    + " " + score + getString(R.string.google_questions), Toast.LENGTH_LONG).show();
+        }
+
+        if (score == 0 )
 
         {
             Toast.makeText(this, name + getString(R.string.your_score_is)
---
+++ app/src/main/java/com/example/user/educateyourselfquiz/Test1stFile.java
@@ -0,0 +1,8 @@
+package com.example.user.educateyourselfquiz;
+
+/**
+ * Created by User on 5.4.2017 г..
+ */
+
+public class Test1stFile {
+}
